ba25fd1d0aa395bd590738c671a8e62b840dafa4
Alex Morgan <ta.srel@hotmail.com>
2026-06-27 03:40:22 -0700

    chore:reorganize constants for better clarity

--- constants.py
+++ constants.py
@@ -1,19 +1,31 @@
-from typing import Final
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+FPS = 60
 
-# Game constants
-GRAVITY: Final[float] = 9.8  # Gravity constant in m/s²
-SPEED_LIMIT: Final[int] = 120  # Speed limit in units
-MAX_HEALTH: Final[int] = 100  # Maximum health of a player
-MIN_HEALTH: Final[int] = 0  # Minimum health of a player
-# Game stages
-class GameStage:
-    MENU: str = 'menu'
-    PLAYING: str = 'playing'
-    PAUSED: str = 'paused'
-    GAME_OVER: str = 'game_over'
+COLORS = {
+    'BLACK': (0, 0, 0),
+    'WHITE': (255, 255, 255),
+    'RED': (255, 0, 0),
+    'GREEN': (0, 255, 0),
+    'BLUE': (0, 0, 255),
+}
 
-class GameSettings:
-    MAX_PLAYERS: Final[int] = 4  # Maximum number of players
-    RESOLUTION: Final[tuple[int, int]] = (1920, 1080)  # Screen resolution
-    VOLUME: Final[float] = 0.5  # Default volume level (0.0 to 1.0)
+PLAYER_SPEED = 5
+ENEMY_SPEED = 3
+PROJECTILE_SPEED = 10
 
+GRAVITY = 0.5
+JUMP_HEIGHT = 10
+
+ASSET_PATHS = {
+    'PLAYER_IMAGE': 'assets/player.png',
+    'ENEMY_IMAGE': 'assets/enemy.png',
+    'BACKGROUND_IMAGE': 'assets/background.png',
+}
+
+LEVELS = {
+    'TUTORIAL': 1,
+    'FOREST': 2,
+    'CAVE': 3,
+    'CASTLE': 4,
+}
