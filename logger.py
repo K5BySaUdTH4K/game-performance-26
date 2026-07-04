a3a617aaa3631023a8ab38971ff505000653f37e
Alex Morgan <ta.srel@hotmail.com>
2026-07-04 03:10:42 -0700

    fix:implement logging for performance optimization

--- logger.py
+++ logger.py
@@ -1,29 +1,31 @@
 import logging
 
-class GameLogger:
-    def __init__(self, name='GameLogger'):
+class CustomLogger:
+    def __init__(self, name):
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.DEBUG)
-        handler = logging.FileHandler('game_log.log')
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
         self.logger.addHandler(handler)
 
-    def log_info(self, message):
-        self.logger.info(message)
-
-    def log_warning(self, message):
-        self.logger.warning(message)
-
-    def log_error(self, message):
-        self.logger.error(message)
-
-    def log_debug(self, message):
-        self.logger.debug(message)
-
-# Example usage:
-# game_logger = GameLogger()
-# game_logger.log_info('Game started successfully!')
-# game_logger.log_warning('Low health warning!')
-# game_logger.log_error('Game crashed unexpectedly!')
-# game_logger.log_debug('Player position updated to (10, 20)')
+    def debug(self, msg):
+        self.logger.debug(msg)
+    
+    def info(self, msg):
+        self.logger.info(msg)
+    
+    def warning(self, msg):
+        self.logger.warning(msg)
+    
+    def error(self, msg):
+        self.logger.error(msg)
+    
+    def critical(self, msg):
+        self.logger.critical(msg)
+    
+# Example usage of the logger
+if __name__ == '__main__':
+    log = CustomLogger('GamePerformanceLogger')
+    log.info('Performance optimization check initiated.')
