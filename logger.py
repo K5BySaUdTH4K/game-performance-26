c32306eb2e8a3f7470159b435c2c97e14e7dac9b
Alex Morgan <ta.srel@hotmail.com>
2026-08-08 13:10:54 +0300

    docs:input validation in main loop

--- logger.py
+++ logger.py
@@ -1,28 +1,29 @@
 import logging
-from logging.handlers import RotatingFileHandler
 
-class Logger:
-    def __init__(self, name='GameLogger', log_file='game.log', max_bytes=1024 * 1024, backup_count=5):
+class GameLogger:
+    def __init__(self, name):
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.DEBUG)
-        handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
 
-    def debug(self, message):
-        self.logger.debug(message)
+    def log(self, level, message):
+        log_func = {
+            'debug': self.logger.debug,
+            'info': self.logger.info,
+            'warning': self.logger.warning,
+            'error': self.logger.error,
+            'critical': self.logger.critical,
+        }
+        log_func.get(level, self.logger.info)(message)
 
-    def info(self, message):
-        self.logger.info(message)
-
-    def warning(self, message):
-        self.logger.warning(message)
-
-    def error(self, message):
-        self.logger.error(message)
-
-    def critical(self, message):
-        self.logger.critical(message)
-
-logger = Logger()  # This will initialize the logger
+if __name__ == '__main__':
+    logger = GameLogger('GamePerformanceLogger')
+    logger.log('info', 'Game started')
+    logger.log('warning', 'Low memory warning')
+    logger.log('error', 'Game crashed!')
+    logger.log('debug', 'Debugging information')
+    logger.log('critical', 'Critical error encountered')
