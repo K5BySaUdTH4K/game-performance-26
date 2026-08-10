be0eb3bab3a639a3b0ccdaf63eac649ca7d25370
Alex Morgan <ta.srel@hotmail.com>
2026-08-10 13:10:28 +0300

    test:refactor game handler structure

--- handler.py
+++ handler.py
@@ -1,33 +1,37 @@
-import time
 import random
+import time
 
 class GameHandler:
-    def __init__(self):
-        self.players = {}
+    def __init__(self, player_name):
+        self.player_name = player_name
+        self.score = 0
+        self.start_time = time.time()
 
-    def add_player(self, player_id):
-        if player_id not in self.players:
-            self.players[player_id] = {'score': 0, 'last_action_time': time.time()}
+    def roll_dice(self):
+        return random.randint(1, 6)
 
-    def update_score(self, player_id, score):
-        if player_id in self.players:
-            current_time = time.time()
-            time_since_last_action = current_time - self.players[player_id]['last_action_time']
-            if time_since_last_action >= 0.1:
-                self.players[player_id]['score'] += score
-                self.players[player_id]['last_action_time'] = current_time
+    def play_turn(self):
+        dice_value = self.roll_dice()
+        print(f'{self.player_name} rolled a {dice_value}.')
+        self.update_score(dice_value)
 
-    def get_scores(self):
-        return {id: data['score'] for id, data in self.players.items()}
+    def update_score(self, value):
+        self.score += value
+        print(f'Current score: {self.score}')
 
-    def simulate_gameplay(self):
-        for player_id in self.players:
-            action_score = random.randint(1, 10)
-            self.update_score(player_id, action_score)
+    def game_duration(self):
+        elapsed = time.time() - self.start_time
+        print(f'Game duration: {elapsed:.2f} seconds')
 
+    def reset_game(self):
+        self.score = 0
+        self.start_time = time.time()
+        print('Game has been reset.')
+
+# Example usage
 if __name__ == '__main__':
-    game = GameHandler()
-    game.add_player('player1')
-    for _ in range(100):
-        game.simulate_gameplay()
-    print(game.get_scores())
+    player = GameHandler('Alice')
+    for _ in range(3):
+        player.play_turn()
+    player.game_duration()
+    player.reset_game()
