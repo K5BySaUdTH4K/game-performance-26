a6f46837b48dcd8ec9628cd926dbfd42cdd166c0
Alex Morgan <ta.srel@hotmail.com>
2026-08-14 01:59:11 +0300

    chore:caching for expensive calculations

--- utils.py
+++ utils.py
@@ -1,37 +1,26 @@
-import random
-import numpy as np
+import time
+import json
+from functools import wraps
 
-def generate_random_number(min_value, max_value):
-    return random.randint(min_value, max_value)
+cache = {}
 
+def memoize(func):
+    @wraps(func)
+    def wrapper(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    return wrapper
 
-def shuffle_list(input_list):
-    shuffled = input_list.copy()
-    random.shuffle(shuffled)
-    return shuffled
+@memoize
+def expensive_calculation(x):
+    time.sleep(2)  # Simulating long computation
+    return x ** 2
 
-
-def calculate_average(numbers):
-    if not numbers:
-        raise ValueError('List of numbers cannot be empty')
-    return sum(numbers) / len(numbers)
-
-
-def normalize_data(data):
-    if not data:
-        raise ValueError('Data cannot be empty')
-    mean = np.mean(data)
-    std_dev = np.std(data)
-    return [(x - mean) / std_dev for x in data]
-
-
-def load_json_file(filepath):
-    import json
-    with open(filepath, 'r') as file:
+def load_json(file_path):
+    with open(file_path, 'r') as file:
         return json.load(file)
 
-
-def save_json_file(filepath, data):
-    import json
-    with open(filepath, 'w') as file:
+def save_json(file_path, data):
+    with open(file_path, 'w') as file:
         json.dump(data, file, indent=4)
