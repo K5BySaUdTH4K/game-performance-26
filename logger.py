aed4f64746bb17e096127d5961acb6c7378d8dbf
Alex Morgan <ta.srel@hotmail.com>
2026-08-14 03:29:13 +0300

    docs:custom logger implementation

--- logger.py
+++ logger.py
@@ -1,23 +1,31 @@
 import logging
 
-# Configure logger
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
+class CustomLogger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
 
-class GameLogger:
-    def __init__(self):
-        self.logs = []
+    def debug(self, msg):
+        self.logger.debug(msg)
 
-    def log_event(self, event_message):
-        if not isinstance(event_message, str) or not event_message:
-            logger.error('Invalid event message: Must be a non-empty string.')
-            return
-        self.logs.append(event_message)
-        logger.info(f'Event logged: {event_message}')
+    def info(self, msg):
+        self.logger.info(msg)
 
-    def get_logs(self):
-        return self.logs
+    def warning(self, msg):
+        self.logger.warning(msg)
 
-    def clear_logs(self):
-        self.logs.clear()
-        logger.info('Logs cleared.')
+    def error(self, msg):
+        self.logger.error(msg)
+
+    def critical(self, msg):
+        self.logger.critical(msg)
+
+if __name__ == '__main__':
+    custom_logger = CustomLogger('MyLogger')
+    custom_logger.info('This is an info message')
+    custom_logger.error('This is an error message')
