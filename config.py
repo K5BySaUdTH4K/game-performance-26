f4c1536234f9292e07829ecfe0c8997315315c71
Alex Morgan <ta.srel@hotmail.com>
2026-08-18 01:15:17 +0300

    docs:configuration loader with defaults

--- config.py
+++ config.py
@@ -1,19 +1,36 @@
+import json
 import os
-import logging
-from logging.handlers import RotatingFileHandler
 
-def setup_logger(log_file='app.log', max_bytes=5e6, backup_count=3):
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    
-    if not logger.handlers:
-        handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+class ConfigLoader:
+    def __init__(self, default_config_path):
+        self.default_config_path = default_config_path
+        self.config_data = self.load_defaults() 
 
-    return logger
+    def load_defaults(self):
+        with open(self.default_config_path, 'r') as file:
+            return json.load(file)
 
-# Example usage:
-# logger = setup_logger()
-# logger.info('This is an info message')
+    def override_with_env(self):
+        for key, value in os.environ.items():
+            if key.startswith('GAME_'):
+                self.set_config_value(key[5:], value)
+
+    def set_config_value(self, key, value):
+        keys = key.split('__')
+        data = self.config_data
+        for k in keys[:-1]:
+            data = data.setdefault(k, {})
+        data[keys[-1]] = value
+
+    def get(self, key, default=None):
+        keys = key.split('__')
+        data = self.config_data
+        for k in keys:
+            data = data.get(k, {})
+        return data if data else default
+
+# Usage
+if __name__ == '__main__':
+    config_loader = ConfigLoader('default_config.json')
+    config_loader.override_with_env()
+    print(config_loader.get('resolution', '1920x1080'))
