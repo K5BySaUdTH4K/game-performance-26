1db0bfbe289f147b00118453a85bee4e13524d06
Alex Morgan <ta.srel@hotmail.com>
2026-08-18 09:30:11 +0300

    fix:centralized game constants definition

--- constants.py
+++ constants.py
@@ -1,26 +1,43 @@
 FPS_LIMIT = 60
-SCREEN_WIDTH = 1920
-SCREEN_HEIGHT = 1080
-PLAYER_SPEED = 5
-GRAVITY = 9.8
-JUMP_HEIGHT = 15
-ITEMS = {
-    'health_potion': {'restore': 50, 'quantity': 10},
-    'mana_potion': {'restore': 30, 'quantity': 5},
-    'elixir': {'restore': 100, 'quantity': 3},
-}
 
-ENEMY_STATS = {
-    'goblin': {'health': 30, 'damage': 5},
-    'troll': {'health': 100, 'damage': 10},
-}
+# Game state constants
+STATE_RUNNING = 'running'
+STATE_PAUSED = 'paused'
+STATE_GAMEOVER = 'game_over'
 
-WEAPON_STATS = {
-    'sword': {'damage': 7, 'durability': 50},
-    'axe': {'damage': 10, 'durability': 30},
-}
+# Color constants
+COLOR_WHITE = (255, 255, 255)
+COLOR_BLACK = (0, 0, 0)
+COLOR_RED = (255, 0, 0)
+COLOR_GREEN = (0, 255, 0)
+COLOR_BLUE = (0, 0, 255)
 
-LEVELS = 5
-DIFFICULTY = 'medium'
-MAX_PLAYERS = 4
-POWER_UPS = ['speed_boost', 'invisibility', 'double_damage']
+# Screen dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Player constants
+PLAYER_START_LIFE = 3
+PLAYER_START_SCORE = 0
+
+# Physics constants
+GRAVITY = 9.81
+FRICTION = 0.1
+
+# Speed settings
+SPEED_SLOW = 5
+SPEED_NORMAL = 10
+SPEED_FAST = 15
+
+# Input constants
+KEY_UP = 'w'
+KEY_DOWN = 's'
+KEY_LEFT = 'a'
+KEY_RIGHT = 'd'
+
+# Miscellaneous
+DEBUG_MODE = True
+SFX_VOLUME = 0.5
+MUSIC_VOLUME = 0.3
+
+# Additional constants can be added here
