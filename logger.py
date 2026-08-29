9abb4298530abdf1c3fda3796faef56c3a4c142d
Alex Morgan <ta.srel@hotmail.com>
2026-08-29 08:20:47 +0300

    test:rotating logger setup for games

--- logger.py
+++ logger.py
@@ -1,29 +1,47 @@
 import logging
 from logging.handlers import RotatingFileHandler
+import os
+from pathlib import Path
 
-class Logger:
-    def __init__(self, name, level=logging.INFO, max_bytes=10 * 1024 * 1024, backup_count=5):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(level)
-        self.handler = RotatingFileHandler('game.log', maxBytes=max_bytes, backupCount=backup_count)
-        self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        self.handler.setFormatter(self.formatter)
-        self.logger.addHandler(self.handler)
-
-    def debug(self, msg):
-        self.logger.debug(msg)
-
+class GamePerformanceLogger:
+    def __init__(self, directory="game_logs", filename="performance.log", max_size_mb=10, backups=4):
+        Path(directory).mkdir(exist_ok=True)
+        full_path = os.path.join(directory, filename)
+        self._logger = logging.getLogger("game-performance-26")
+        if self._logger.hasHandlers():
+            self._logger.handlers.clear()
+        self._logger.setLevel(logging.DEBUG)
+        file_handler = RotatingFileHandler(
+            full_path,
+            maxBytes=max_size_mb * 1024 * 1024,
+            backupCount=backups,
+            encoding="utf-8"
+        )
+        file_handler.setFormatter(
+            logging.Formatter(
+                fmt="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
+                datefmt="%H:%M:%S"
+            )
+        )
+        self._logger.addHandler(file_handler)
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        stream_handler.setFormatter(
+            logging.Formatter("%(levelname)s: %(message)s")
+        )
+        self._logger.addHandler(stream_handler)
+    def log(self, level, msg):
+        self._logger.log(level, msg)
     def info(self, msg):
-        self.logger.info(msg)
-
+        self._logger.info(msg)
     def warning(self, msg):
-        self.logger.warning(msg)
-
+        self._logger.warning(msg)
     def error(self, msg):
-        self.logger.error(msg)
+        self._logger.error(msg)
+    def debug(self, msg):
+        self._logger.debug(msg)
+    @property
+    def logger(self):
+        return self._logger
 
-    def critical(self, msg):
-        self.logger.critical(msg)
-
-logger = Logger('GameLogger')
-logger.info('Logger initialized with rotation')
+performance_logger = GamePerformanceLogger()
