de7a97ae891e226b6b8eac95869c67c33a64b203
Alex Morgan <ta.srel@hotmail.com>
2026-09-09 01:06:15 +0300

    chore:log rotation setup for performance metrics

--- logger.py
+++ logger.py
@@ -1,47 +1,36 @@
 import logging
 from logging.handlers import RotatingFileHandler
-import os
+import sys
 from pathlib import Path
 
-class GamePerformanceLogger:
-    def __init__(self, directory="game_logs", filename="performance.log", max_size_mb=10, backups=4):
-        Path(directory).mkdir(exist_ok=True)
-        full_path = os.path.join(directory, filename)
-        self._logger = logging.getLogger("game-performance-26")
-        if self._logger.hasHandlers():
-            self._logger.handlers.clear()
-        self._logger.setLevel(logging.DEBUG)
-        file_handler = RotatingFileHandler(
-            full_path,
-            maxBytes=max_size_mb * 1024 * 1024,
-            backupCount=backups,
-            encoding="utf-8"
-        )
-        file_handler.setFormatter(
-            logging.Formatter(
-                fmt="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
-                datefmt="%H:%M:%S"
-            )
-        )
-        self._logger.addHandler(file_handler)
-        stream_handler = logging.StreamHandler()
-        stream_handler.setLevel(logging.INFO)
-        stream_handler.setFormatter(
-            logging.Formatter("%(levelname)s: %(message)s")
-        )
-        self._logger.addHandler(stream_handler)
-    def log(self, level, msg):
-        self._logger.log(level, msg)
-    def info(self, msg):
-        self._logger.info(msg)
-    def warning(self, msg):
-        self._logger.warning(msg)
-    def error(self, msg):
-        self._logger.error(msg)
-    def debug(self, msg):
-        self._logger.debug(msg)
-    @property
-    def logger(self):
-        return self._logger
+LOG_DIR = Path('logs')
+LOG_DIR.mkdir(exist_ok=True)
 
-performance_logger = GamePerformanceLogger()
+def setup_performance_logger(name: str = 'game_engine') -> logging.Logger:
+    """Initializes high-performance game state logging"""
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        '[%(asctime)s] [%(levelname)s] [%(name)s] >> %(message)s',
+        datefmt='%H:%M:%S'
+    )
+
+    file_handler = RotatingFileHandler(
+        LOG_DIR / f'{name}.log',
+        maxBytes=5 * 1024 * 1024,
+        backupCount=3
+    )
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+
+    if not logger.handlers:
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
+    return logger
+
+# Global instance for performance telemetry
+perf_log = setup_performance_logger('perf_tracker')
